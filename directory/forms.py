1d80e4b073f7afb641b90d829bd71aba24c1a413
Miguel Romero <Miguelrom@users.noreply.github.com>
2022-06-17 18:54:10 -0700

    Add placeholders and label names to some fields.

--- directory/forms.py
+++ directory/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
 
 from .models import Contact
 
@@ -9,12 +10,21 @@ class ContactForm(ModelForm):
 
         self.fields['phone_number'].widget.attrs.update({'minlength': '10'})
 
+        self.fields['email'].widget.attrs.update({'placeholder': 'username@email.com'})
+        self.fields['phone_number'].widget.attrs.update({'placeholder': '5555555555'})
+
+
         for field in self.fields.values():
             field.widget.attrs.update({'class': 'form-control'})
 
     class Meta:
         model = Contact
         fields = ("name", 'last_name', 'email', 'phone_number', 'company')
+
+        labels = {
+            'name': _('Name(s)'),
+            'last_name': _('Last name(s)'),
+        }
 
 
 
