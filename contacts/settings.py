69f4a75195cede89e0c24445dcd108d35f58b4ca
Miguel Romero <Miguelrom@users.noreply.github.com>
2022-07-13 18:48:36 -0700

    Add database url configuration for heroku.

--- contacts/settings.py
+++ contacts/settings.py
@@ -133,3 +133,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
